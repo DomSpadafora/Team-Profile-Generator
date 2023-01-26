eb15ba762fd508b0c78c2f250a5b446213be58d8
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-01-25 21:44:30 -0500

    finished employee test page

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,8 +1,8 @@
 //require employee
 const Employee = require('../lib/Employee');
 
-//write a test to see if you can instantiate an Employee instance 
-test('Can instantiate Employee instance', () => {
+//write a test to see if you can instantiate an Employee instance via constructor values 
+test('Can instantiate Employee instance with constructor values', () => {
 //variable for new employee with name, id, & email
     const employee = new Employee('John', '45689', 'johndoe@gmail.com');
     expect(employee.name).toBe('John');
@@ -11,6 +11,22 @@ test('Can instantiate Employee instance', () => {
 });
 
 
-test('Can set name via constructor arguments', () => {
-    
-})
+test('Can get name via constructor arguments', () => {
+    const employee = new Employee('John', '45689', 'johndoe@gmail.com');
+    expect(employee.getName()).toBe('John')    
+});
+
+test('Can get id via constructor arguments', () => {
+    const employee = new Employee('John', '45689', 'johndoe@gmail.com');
+    expect(employee.getId()).toBe('45689')    
+});
+
+test('Can get email via constructor arguments', () => {
+    const employee = new Employee('John', '45689', 'johndoe@gmail.com');
+    expect(employee.getEmail()).toBe('johndoe@gmail.com')    
+});
+
+test('Can get the employee role via constructor arguments', () => {
+    const employee = new Employee('John', '45689', 'johndoe@gmail.com');
+    expect(employee.getEmployee()).toBe('Employee')    
+});
