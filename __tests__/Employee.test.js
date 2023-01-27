ff292b679e8ae2924a1e67a0435037165aa7eeb7
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-01-26 20:59:40 -0500

    cleaned up some of the test files, deleted fluff

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,32 +1,28 @@
 //require employee
 const Employee = require('../lib/Employee');
 
+//variable for new employee with name, id, & email
+const employee = new Employee('John', '45689', 'johndoe@gmail.com');
+
 //write a test to see if you can instantiate an Employee instance via constructor values 
 test('Can instantiate Employee instance with constructor values', () => {
-//variable for new employee with name, id, & email
-    const employee = new Employee('John', '45689', 'johndoe@gmail.com');
     expect(employee.name).toBe('John');
     expect(employee.id).toBe('45689');
     expect(employee.email).toBe('johndoe@gmail.com');
 });
 
-
 test('Can get name via getName method', () => {
-    const employee = new Employee('John', '45689', 'johndoe@gmail.com');
     expect(employee.getName()).toBe('John')    
 });
 
 test('Can get id via getId method', () => {
-    const employee = new Employee('John', '45689', 'johndoe@gmail.com');
     expect(employee.getId()).toBe('45689')    
 });
 
 test('Can get email via getEmail method', () => {
-    const employee = new Employee('John', '45689', 'johndoe@gmail.com');
     expect(employee.getEmail()).toBe('johndoe@gmail.com')    
 });
 
 test('Can get the employee role via getRole method', () => {
-    const employee = new Employee('John', '45689', 'johndoe@gmail.com');
     expect(employee.getRole()).toBe('Employee')    
 });
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,40 +1,17 @@
 //require engineer 
 const Engineer = require('../lib/Engineer');
 
+const engineer = new Engineer('John', '45689', 'johndoe@gmail.com', 'johndoe25');
 
-
-//write a test to see if you can instantiate an engineer instance via constructor values 
-test('Can instantiate engineer instance with constructor values', () => {
-//variable for new employee with name, id, email, & github username
-    const engineer = new Engineer('John', '45689', 'johndoe@gmail.com', 'johndoe25');
-    expect(engineer.name).toBe('John');
-    expect(engineer.id).toBe('45689');
-    expect(engineer.email).toBe('johndoe@gmail.com');;
-    expect(engineer.github).toBe('johndoe25');
+test('Add new constructor for github user name', () => {
+    expect(engineer.github).toBe('johndoe25')    
 });
 
-
-test('Can get name via getName method', () => {
-    const engineer = new Engineer('John', '45689', 'johndoe@gmail.com', 'johndoe25');
-    expect(engineer.getName()).toBe('John')    
-});
-
-test('Can get id via getId method', () => {
-    const engineer = new Engineer('John', '45689', 'johndoe@gmail.com', 'johndoe25');
-    expect(engineer.getId()).toBe('45689')    
-});
-
-test('Can get email via getEmail method', () => {
-    const engineer = new Engineer('John', '45689', 'johndoe@gmail.com', 'johndoe25');
-    expect(engineer.getEmail()).toBe('johndoe@gmail.com')    
-});
 
 test('Can get GitHub username via getEmail method', () => {
-    const engineer = new Engineer('John', '45689', 'johndoe@gmail.com', 'johndoe25');
     expect(engineer.getGithub()).toBe('johndoe25')    
 });
 
 test('Can get the engineer role via getRole method', () => {
-    const engineer = new Engineer('John', '45689', 'johndoe@gmail.com', 'johndoe25');
     expect(engineer.getRole()).toBe('Engineer')    
 });
